6784b6ece0896a390d4929782422be108a0ee545
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-05-24 16:36:08 +0530

    added alert

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -38,6 +38,7 @@ export class LoginComponent implements OnInit {
         console.log(response);
         this.userAuthService.setRoles(response.userDetails.role);
         this.userAuthService.setToken(response.jwt);
+        alert('Login Successful');
         const role = this.userAuthService.getRoles();
         if (role === 'ROLE_Farmer') {
           this.router.navigate(['/Home']);
