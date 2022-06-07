a03019b2a709968de9d1c869c79ae1bed9971cca
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-06-07 19:59:33 +0530

    Added Search option for Crops

--- src/app/Components/CropsComponent/CropsComponent.component.ts
+++ src/app/Components/CropsComponent/CropsComponent.component.ts
@@ -9,10 +9,12 @@ import { CropServiceService } from '../../services/CropService.service';
 })
 export class CropsComponentComponent implements OnInit {
   public crops: any = [];
+  public  orginalcrop: any = [];
   public clicked = true;
   constructor(private _cropservice: CropServiceService, private _Router: Router, private route: ActivatedRoute) { }
   ngOnInit() {
-    this._cropservice.getCrops().subscribe(data => this.crops = data,
+    this._cropservice.getCrops().subscribe(data => {this.crops = data;
+      this.orginalcrop=this.crops;},
       error => {
         console.log(error);
         Swal.fire({
@@ -23,4 +25,22 @@ export class CropsComponentComponent implements OnInit {
       });
   }
 
+  reset() {
+    this.crops = this.orginalcrop;
+  }
+  filter(value: any) {
+    // console.log(value);
+    if (value && value.trim() !== '') {
+      this.crops = this.crops.filter((item: any) => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1);
+      if (this.crops.length == 0) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'No crop found!',
+          timer: 5000
+      });
+      this.crops = this.orginalcrop;
+    }
+    }
+  }
 }
