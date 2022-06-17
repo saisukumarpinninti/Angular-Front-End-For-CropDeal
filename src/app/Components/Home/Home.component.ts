06127c6f4dc6351126863c94c1b34ce7b46cad7b
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-06-17 20:32:43 +0530

    made some changes and added welcome message

--- src/app/Components/Home/Home.component.ts
+++ src/app/Components/Home/Home.component.ts
@@ -8,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  v='';
+  t = new Date();
+  n = this.t.getHours();
   ngOnInit() {
+    var v = "welcome";
+    if(this.n<4){
+       this.v = "Night  Owl";   
+    }
+    if( this.n > 4 && this.n <= 11){
+      this.v = "Good Morning"; 
+    }
+    if( this.n > 11 && this.n <= 15){
+      this.v = "Good AfterNoon"; 
+    }
+    if( this.n >15 && this.n <= 21){
+      this.v =  "Good Evening";
+    }
+    if( this.n >21 && this.n <= 24){
+      this.v =  "Good Evening";
+    }
+    return v;
   }
+
 
 }
